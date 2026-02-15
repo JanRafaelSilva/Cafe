b9820545826b5bb4ecc176cd43a6e28bb58be2e3
Burnout <jansilvavieira16@gmail.com>
2026-02-15 17:04:43 -0300

    placar

--- Assets/Scripts/Placar.cs
+++ Assets/Scripts/Placar.cs
@@ -4,17 +4,28 @@ public class Placar : MonoBehaviour
 {
     public int cafe;
     public Player player;
+    public int points = 0;
+    public int pontosTime;
+    public float time = 60f;
     void Awake()
     {
         player = GetComponent<Player>();
     }
-
-   public void coletaCafe(int value)
+    private void Update()
     {
-
+        time -= Time.deltaTime;
+        if(pontosTime == 2)
+        {
+            time += 30f;
+            pontosTime = 0;
+        }
+        
     }
-    public void pontos(int value)
+
+    public void coletaCafe()
     {
-
+        pontosTime++;
+        points = +500;
     }
+
 }
