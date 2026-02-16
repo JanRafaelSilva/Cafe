9043278f41ae529295944708d464d89c0c51bb0c
vkobo <vinicius.kn@hotmail.com>
2026-02-15 22:52:21 -0300

    mapa

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    Vector3 offset = new Vector3 (0,2,-10);
+    Vector3 offset = new Vector3 (0,0,-10);
     public Vector3 velocity;
     public float smoothTime = 0.2f;
 
